5e1ab501b4ba1d0ddfd6bd7dc3dc63670e355b3b
Mahan Rad <mahorad@yahoo.com>
2017-08-22 19:40:44 +0200

    add timber to gradle file

---
+++ app/src/main/java/mahorad/com/android_dagger/util/MyActivityDependency.java
@@ -0,0 +1,13 @@
+package mahorad.com.daggerstructure.util;
+
+/**
+ * Created by Mahan Rad on 2017-08-21.
+ */
+
+public class MyActivityDependency {
+
+    public int getHashCode() {
+        return System.identityHashCode(this);
+    }
+
+}
